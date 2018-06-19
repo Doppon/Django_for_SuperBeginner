47afeec8eb611b67974c3797bdabae70cfc61326
Doppon <zaxpkuraudo7777@gmail.com>
2018-06-20 02:37:28 +0900

    Groupモデルを追加

--- sns/models.py
+++ sns/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Group(models.Model):
+	owner = models.ForeignKey(User, on_delete=models.CASCADE, \
+		related_name='group_owner')
+	title = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.title
+		
