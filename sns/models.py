879de5552ee870950cdaed0ed5c7f76cdc1d2b78
Doppon <zaxpkuraudo7777@gmail.com>
2018-06-20 02:55:23 +0900

    Messageクラスを追加

--- sns/models.py
+++ sns/models.py
@@ -1,6 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Message(models.Model):
+	owner = models.ForeignKey(User, on_delete=models.CASCADE, \
+		related_name='message_owner')
+	group = models.ForeignKey('Group', on_delete=models.CASCADE)
+	content = models.TextField(max_length=1000)
+	share_id = models.IntegerField(default=-1)
+	good_count = models.IntegerField(default=0)
+	share_count = models.IntegerField(default=0)
+	pub_date = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return str(self.content) + '/' + str(self.owner)
+
+	def get_share(self):
+		return Message.objects.get(id=self.share_id)
+
+	class Meta:
+		ordering = ('-pub_date')
+	
+
+
 class Group(models.Model):
 	owner = models.ForeignKey(User, on_delete=models.CASCADE, \
 		related_name='group_owner')
@@ -28,4 +49,4 @@ class Good(models.Model):
 	def __str__(self):
 		return 'good for "' + str(self.message) + '" (by ' + \
 			str(self.owner) + ')'
-			
+
