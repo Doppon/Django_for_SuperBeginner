e043f84ed063aa2b09f10809ba6dee25ae2c5b21
Doppon <zaxpkuraudo7777@gmail.com>
2018-06-29 13:23:03 +0900

    ユーザー情報の取得処理を追加

--- sns/views.py
+++ sns/views.py
@@ -2,3 +2,14 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
+
+from .models import Message, Friend, Group, Good
+
+from django.db.models import Q
+from django.contrib.auth.decorators import login_required
+
+# indexのビュー関数
+@login_required(login_url='/admin/login/')
+def index(request):
+  #publicのuserを取得
+  (public_user, public_group) = get_public()
