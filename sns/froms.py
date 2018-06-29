c80ba2c04ee3619b9ac0de27084ee57d8f3875df
Doppon <zaxpkuraudo7777@gmail.com>
2018-06-29 13:18:06 +0900

    必要なライブラリをインポート

---
+++ sns/froms.py
@@ -0,0 +1,3 @@
+from django import forms
+from.models import Message, Group, Friend, Good
+from django.contrib.auth.models import User
--- sns/views.py
+++ sns/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
