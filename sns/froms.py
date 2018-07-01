3e6762dc9c44d9cc7055a78109be6344aff4e746
Doppon <zaxpkuraudo7777@gmail.com>
2018-07-01 19:45:59 +0900

    formsの擬似コードを追加

--- sns/froms.py
+++ sns/froms.py
@@ -1,3 +1,23 @@
 from django import forms
 from.models import Message, Group, Friend, Good
 from django.contrib.auth.models import User
+
+# Messageのフォーム(使うときに記述)
+
+# Groupのフォーム(使うときに記述)
+
+# Friendのフォーム(使うときに記述)
+
+# Goodのフォーム(使うときに記述)
+
+# 検索フォーム
+
+# Groupのチェックボックスフォーム
+
+# Groupの選択メニューフォーム
+
+# Friendのチェックボックスフォーム
+
+# Group作成フォーム
+
+# 投稿フォーム
